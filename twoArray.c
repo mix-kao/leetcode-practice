f29d35f6d57b7f1fcd2b3be6e2f4658bfd2f7c0d
mix <mix.kao@gmail.com>
2019-08-13 11:38:24 +0800

    Add findMissingByHash to reduce the time complexity

--- twoArray.c
+++ twoArray.c
@@ -1,19 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define HASH_SIZE 6
 
 // Function for finding
 // elements which are there
 // in a[] but not in b[].
 void findMissing(int a[], int b[], size_t a_size, size_t b_size) {
     int same_flag = 0;
+    int count = 0;
     for(int i=0; i<a_size; i++) {
         same_flag = 0;
         for(int j=0; j<b_size; j++) {
+            count++;
             if(a[i] == b[j]) { same_flag = 1; break;}
         }
         if(!same_flag) printf("%d\n", a[i]);
     }
+    printf("count = %d\n", count);
+}
+
+void findMissing1(int* a, int* b, size_t a_size, size_t b_size) {
+    int same_flag = 0;
+    int count = 0;
+    for(int i=0; i<a_size; i++) {
+        same_flag = 0;
+        for(int j=0; j<b_size; j++) {
+            count++;
+            if(*(a+i) == *(b+j)) { same_flag = 1; break;}
+        }
+        if(!same_flag) printf("%d\n", *(a+i));
+    }
+    printf("count = %d\n", count);
+
+}
+
+void findMissingByHash(int* a, int* b, size_t a_size, size_t b_size) {
+    int same_flag = 0;
+    int b_hash[HASH_SIZE];
+    int remainder;
+    int count = 0;
+    memset(b_hash, -1, sizeof(b_hash));
+    for(int j=0; j<b_size; j++) {
+        remainder = *(b+j)%(HASH_SIZE);
+        b_hash[remainder] = *(b+j);
+        count++;
+    }
+    for(int i=0; i<a_size; i++) {
+        count++;
+        same_flag = 0;
+        remainder = *(a+i)%HASH_SIZE;
+        if(b_hash[remainder]!=-1) same_flag=1;
+        if(!same_flag) printf("%d\n", *(a+i));
+    }
+    printf("count = %d\n", count);
 
 }
 
@@ -26,8 +66,9 @@ int main(void) {
     size_t a_size = sizeof(a)/sizeof(a[0]);
     size_t b_size = sizeof(b)/sizeof(b[0]);
 
-    printf("sizeof a=%ld, b=%ld\n", a_size, b_size);
     findMissing(a, b, a_size, b_size);
+    findMissing1(a, b, a_size, b_size);
+    findMissingByHash(a, b, a_size, b_size);
 
     return 0;
 }
