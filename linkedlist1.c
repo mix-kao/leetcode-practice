cb70f12644b32ffbdd19a57e722ee3c7e52b13bb
mix <mix.kao@gmail.com>
2019-08-15 15:32:51 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      modified:   linkedlist1.c

--- linkedlist1.c
+++ linkedlist1.c
@@ -22,24 +22,51 @@ void addNode(node* head, char* name) {
     }
 }
 
+void delNode(node* curr, char* name) {
+    node* prev = curr;
+    printf("1 delNode %s\n", name);
+    while(curr) {
+        printf("%s %s\n", curr->name, name);
+        if(!strcmp(curr->name, name)) {
+            printf("%s \n", curr->name);
+            prev->next = curr->next;
+            curr->next = NULL;
+            break;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+    free(curr);
+    printf("2 delNode=====\n");
+}
+
 static char student_name_data[5][10] = {"abc", "def", "ghi", "jkl", "mno"};
 
 int main(void) {
     node s1 = {"Mary", 10000, 100};
-    node s2 = {"Jonb", 10001, 100};
+    node *s2 = (node*)malloc(sizeof(node));
+    strcpy(s2->name, "Jonb"); s2->num = 10003;s2->score = 77;s2->next=NULL;
     node* head = &s1;
-    s1.next = &s2;
-    s2.next = NULL;
+    node* top = head;
+    s1.next = s2;
     
     int student_num = sizeof(student_name_data)/sizeof(student_name_data[0]);
     printf("%ld, %ld \n", sizeof(student_name_data), sizeof(student_name_data[0]));
     for(int i=0; i<student_num; i++) {
         addNode(head, student_name_data[i]);
     }
-    while(head) {
-        printf("node name = %s, num = %d, score = %d\n", head->name, head->num, head->score);
-        head = head->next;
+    while(top) {
+        printf("node name = %s, num = %d, score = %d\n", top->name, top->num, top->score);
+        top = top->next;
     }
-    
+    delNode(head, "mno");
+    delNode(head, "abc");
+    delNode(head, "Jonb");
+    top = head;
+    while(top) {
+        printf("node name = %s, num = %d, score = %d\n", top->name, top->num, top->score);
+        top = top->next;
+    }
+   
     return 0;
 }
